ef6a5d9a02984559375b2f6bc3dd0ce46ec417cf
bagge <bagge@qq.com>
2015-07-06 23:45:42 +0800

    Signed-off-by: bagge <bagge@qq.com>

---
+++ src/com/job/action/IndexAction.java
@@ -0,0 +1,23 @@
+package com.job.action;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Component("indexAction")
+@Scope("prototype")
+public class IndexAction extends ActionSupport{
+
+
+	private static final long serialVersionUID = 100000000000000L;
+
+	Logger logger = Logger.getLogger(this.getClass().getName());
+
+	@Override
+	public String execute() throws Exception {
+		logger.info("主页访问了");
+		return "index";
+	}
+}
---
+++ src/com/job/service/IndexServlet.java
@@ -0,0 +1,44 @@
+package com.job.service;
+
+import java.io.IOException;
+
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.write("this is index get");
+		System.out.println("this is doget");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("this is dopost");
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1)
+			throws ServletException, IOException {
+		
+		System.out.println("this is service");
+		super.service(arg0, arg1);
+	}
+
+	
+
+}
